c1b5beee040adfc571a2e3480c0cf075597214f4
Oluwadolapo Olomo <oluwadolapoolomo@gmail.com>
2022-10-07 23:49:04 -0400

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+/**
+* @author
+* @function Header
+**/
+
+export const Header = (props) => {
+  return(
+    <header className="header">
+
+        <nav className='headerMenu'>
+            <a href="#">Women's</a>
+            <a href="#">Men's</a>
+            <a href="#">On the Street</a>
+            <a href="#">The Catwalk</a>
+            <a href="#">Adwatch</a>
+            <a href="#">About Us</a>
+        </nav>
+        <div>
+            Media Links
+        </div>
+    </header>
+   )
+
+ }
+ export default Header
